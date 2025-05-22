2d73ac73a23e8e1747baba3e3afd9812f2779df9
Edgar Hernandez <edgar.hdjz@gmail.com>
2025-05-22 16:50:56 -0600

    Testing remote state

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "cinammon-org" # app.terraform.io
+    workspaces {
+      name = "learn-terraform-aws"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
