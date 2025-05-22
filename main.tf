a0c008bde635cff675da04659c5df6a3f543942d
Edgar Hernandez <edgar.hdjz@gmail.com>
2025-05-22 14:13:48 -0600

    working with variables

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
@@ -18,6 +18,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "app-server-instance"
+    Name = var.instance_name
   }
 }
+
